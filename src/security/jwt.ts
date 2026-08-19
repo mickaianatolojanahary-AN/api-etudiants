e98770c41c6afad07fe0ae5cb4d9b46741240dfa
Michaia Natolojanahary <hei.michaia.2@gmail.com>
2026-08-19 14:21:03 +0300

    Mise en place de l authentification et de la structure du backend

--- src/security/jwt.ts
+++ src/security/jwt.ts
@@ -1,15 +1,14 @@
 import jwt from 'jsonwebtoken';
 
-// Une clé secrète (dans un vrai projet, on la mettrait dans un fichier .env)
+
 const JWT_SECRET = 'votre_cle_secrete_super_securisee';
 
 export const jwtUtils = {
-    // Générer un token pour un utilisateur
+    
     generateToken: (userId: number, email: string) => {
         return jwt.sign({ userId, email }, JWT_SECRET, { expiresIn: '1h' });
     },
 
-    // Vérifier un token (middleware pour protéger les routes)
     verifyToken: (token: string) => {
         try {
             return jwt.verify(token, JWT_SECRET);
